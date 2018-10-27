7b9f4e299f81fbfb031a4621cf91257768240b01
John Zhang <johnz0923@gmail.com>
2018-10-27 23:45:59 +0100

    added solution for p20

---
+++ easy/p20_valid_parentheses.py
@@ -0,0 +1,38 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+"""
+import re
+
+
+class Solution:
+    def is_valid(self, s):
+        if s != "":
+            result = False
+            queue = list()
+            for i in s:
+                if i == "{":
+                    result = True
+                    queue.append("}")
+                elif i == "(":
+                    result = True
+                    queue.append(")")
+                elif i == "[":
+                    result = True
+                    queue.append("]")
+                elif i == "}" or i == ")" or i == "]":
+                    if len(queue) != 0 and queue[len(queue) - 1] == i:
+                        queue.pop()
+                    else:
+                        return False
+            if len(queue) != 0:
+                return False
+            else:
+                return result
+        else:
+            return True
